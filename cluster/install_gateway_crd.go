acf88a20220d3bf6ade37ea82e7997566ceb2fff
nxy7 <lolnoxy@gmail.com>
2024-02-21 12:57:37 +0100

    gateway crd installation fix
    Signed-off-by: nxy7 <lolnoxy@gmail.com>

--- cluster/install_gateway_crd.go
+++ cluster/install_gateway_crd.go
@@ -2,6 +2,8 @@ package cluster
 
 import (
 	"k0d/utils"
+	"log"
+	"os"
 	"os/exec"
 	"sync"
 )
@@ -11,19 +13,24 @@ func InstallGatewayCrds() {
 	s.Start()
 	defer s.Stop()
 	manifests := []string{
-		"kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/standard/gateway.networking.k8s.io_gatewayclasses.yaml",
-		"kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/standard/gateway.networking.k8s.io_gateways.yaml",
-		"kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/standard/gateway.networking.k8s.io_httproutes.yaml",
-		"kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/standard/gateway.networking.k8s.io_referencegrants.yaml",
-		"kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/experimental/gateway.networking.k8s.io_grpcroutes.yaml",
-		"kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml",
+		"https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/standard/gateway.networking.k8s.io_gatewayclasses.yaml",
+		"https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/standard/gateway.networking.k8s.io_gateways.yaml",
+		"https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/standard/gateway.networking.k8s.io_httproutes.yaml",
+		"https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/standard/gateway.networking.k8s.io_referencegrants.yaml",
+		"https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/experimental/gateway.networking.k8s.io_grpcroutes.yaml",
+		"https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.0.0/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml",
 	}
 	wg := sync.WaitGroup{}
 	for _, manifest := range manifests {
 		wg.Add(1)
 		go func(manifest string) {
-			err := exec.Command("kubectl", "apply", "-f", manifest).Run()
+			cmd := exec.Command("kubectl", "apply", "-f", manifest)
+			stderr := os.Stderr
+			cmd.Stderr = stderr
+
+			err := cmd.Run()
 			if err != nil {
+				log.Println(manifest)
 				panic(err)
 			}
 			wg.Done()
